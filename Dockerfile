77037f15a4b2d047f98437c2df05e4683432592d
Rignchen <rinchendemort@gmail.com>
2024-06-18 12:29:31 +0200

    feat: use ast-grep to get block with wanted comment

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,15 @@
 FROM rust:1.79-alpine
+RUN apk add build-base
+RUN cargo install ast-grep --locked
 RUN apk add fd
+
+FROM alpine:3.20
+
+COPY --from=0 /usr/local/cargo/bin/ast-grep /usr/local/bin/ast-grep
+COPY --from=0 /usr/bin/fd /usr/local/bin/fd
+
+WORKDIR /app
+COPY Cargo.* ./
+COPY ast-grep-rules ast-grep-rules
+COPY sgconfig.yml sgconfig.yml
 
--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,38 @@ fn main() {
 
     for extension in extensions {
         // get all files with the given extension in the path
-        let files = run_command(&format!("fd . {} -e {} --type f", path, extension));
-        let files = files.split('\n').collect::<Vec<&str>>();
-        let files = files
-            .iter()
-            .filter(|&x| !x.is_empty())
-            .collect::<Vec<&&str>>();
+        let files = get_files(path, extension);
         for file in files {
-            println!("{}", file);
+            let parts = get_removable_parts(extension, &file);
+            println!("{}", parts);
         }
     }
 }
-// ast-grep scan --rule ast-grep-rules/rust.yaml example/arrays.rs --json=stream
+
+/// get the list of files with the given extension in the given path
+fn get_files(path: &str, extension: &str) -> Vec<String> {
+    let files = run_command(&format!("fd . {} -e {} --type f", path, extension));
+    let files = files.split('\n').collect::<Vec<&str>>();
+    let files = files
+        .iter()
+        .filter(|&x| !x.is_empty())
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+    files
+}
+
+/// get the positions of the comments and block who define the beginning of the part to remove
+fn get_removable_parts(extension: &str, file: &str) -> String {
+    let json = run_command(&format!(
+        "ast-grep scan --rule ast-grep-rules/{}.yaml {} --json=stream",
+        extension, file
+    ));
+    json
+}
 
 /// run a bash command and return the output
 fn run_command(command: &str) -> String {
-    let output = Command::new("bash")
+    let output = Command::new("sh")
         .arg("-c")
         .arg(command)
         .output()
