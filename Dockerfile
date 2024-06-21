9fbb77dc754b058cda3d7ab052ca9fc3d3076fcd
Nils Hofstetter <119439839+Rignchen@users.noreply.github.com>
2024-06-21 14:15:45 +0200

    feat: remove code after comments in rust (#8)
    feat: get the list of files ending with an extension in a path
    
    refactor: run cargo fmt and cargo clippy
    
    feat: use ast-grep to get block with wanted comment
    
    refactor: move project to /etc/jobtrek/sw
    
    perf: make get_files and run_command 1 line long
    
    feat: parse json output from ast-grep
    
    refactor: use structstruck to nest structures to look like the json
    
    (strstruct)[https://docs.rs/structstruck/latest/structstruck/] is a crate made to be able to nest structures inside each other, it makes it easier to see the json's structure, take less places and allow to have only one declarative tags to give attributes to every part of the nested struct
    
    refactor: make all structure field snake_case
    
    feat: get the lines number of the lines to remove
    
    fix: select definition scope with the other scopes
    
    feat: remove the wanted code from the file
    
    feat: add toto!() macro to each functions
    
    perf: remove variable to querry data directly when usefull
    
    feat: update dockerfile
    
    feat: indent replacing text according to the comment's indentation
    
    style: comments the Dockerfile
    
    feat: create a developement version of the dockerfile
    
    test: add the most relevent test done to the project files

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM rust:1.79-alpine
+WORKDIR /etc/jobtrek/sw
+
+# Install the dependencies
+RUN apk add build-base
+RUN cargo install ast-grep --locked
+RUN apk add --no-cache fd musl-dev
+
+# Copy the project files
+COPY sgconfig.yml sgconfig.yml
+COPY ast-grep-rules ast-grep-rules
+COPY Cargo.* ./
+COPY src src
+
+# Build the project
+RUN cargo build --release
+
+# Final image
+FROM alpine:3.20
+COPY --from=0 /etc/jobtrek/sw/target/release/sw /usr/local/bin/sw
+COPY --from=0 /etc/jobtrek/sw/sgconfig.yml /etc/jobtrek/sw/sgconfig.yml
+COPY --from=0 /etc/jobtrek/sw/ast-grep-rules /etc/jobtrek/sw/ast-grep-rules
+COPY --from=0 /usr/local/cargo/bin/ast-grep /usr/local/bin/ast-grep
+RUN apk add --no-cache fd
+COPY test test
+ENTRYPOINT ["sw"]
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,105 @@
+use serde::{Deserialize, Serialize};
+use std::process::Command;
+
+// structure of the json returned by ast-grep (only the useful parts)
+structstruck::strike! {
+    #[strikethrough[derive(Serialize, Deserialize, Debug)]]
+    struct Program {
+        text: String,
+        range: struct {
+            start: struct {
+                line: u16,
+                column: u16,
+            },
+            end: struct {
+                line: u16,
+                column: u16,
+            }
+        },
+        #[serde(rename = "metaVariables")]
+        meta_variables: struct {
+            single: struct {
+                #[serde(rename = "COMMENT")]
+                comment: struct {
+                    text: String,
+                    // here we can directly refer to the range struct as we allready defined it
+                    range: Range
+                }
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = "./test";
+    let extensions = "rs,php,js,ts,java".split(',').collect::<Vec<&str>>();
+    for extension in extensions {
+        let files = get_files(path, extension);
+        for file in files {
+            let parsed = get_removable_parts(extension, &file);
+            if parsed.is_empty() {
+                continue;
+            }
+            remove_parts(&file, &parsed, "todo!()")
+                .unwrap_or_else(|e| eprintln!("failed to remove parts from {}: {}", file, e));
+        }
+    }
+}
+
+/// remove the parts of the file that are defined in the given list of programs
+/// parts are removed in reverse order to avoid changing the line numbers of the other parts
+fn remove_parts(file: &str, parts: &[Program], replace_with: &str) -> std::io::Result<()> {
+    let content = std::fs::read_to_string(file)?;
+    let mut content = content.lines().map(String::from).collect::<Vec<String>>();
+    for part in parts.iter().rev() {
+        content.splice(
+            (part.meta_variables.single.comment.range.end.line as usize + 1)
+                ..=(part.range.end.line as usize - 1),
+            indent(
+                replace_with,
+                part.meta_variables.single.comment.range.start.column as usize,
+            ),
+        );
+    }
+    std::fs::write(file, content.join("\n"))?;
+    Ok(())
+}
+
+/// add the right amount of spaces to the beginning of each lines
+fn indent(lines: &str, spaces: usize) -> Vec<String> {
+    lines
+        .lines()
+        .map(|x| format!("{: >width$}{}", "", x, width = spaces))
+        .collect()
+}
+
+/// get the list of files with the given extension in the given path
+fn get_files(path: &str, extension: &str) -> Vec<String> {
+    run_command(&format!("fd . {} -e {} --type f", path, extension))
+        .split('\n')
+        .filter(|&x| !x.is_empty())
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+}
+
+/// get the positions of the comments and block who define the beginning of the part to remove
+fn get_removable_parts(extension: &str, file: &str) -> Vec<Program> {
+    serde_json::from_str(&run_command(&format!(
+        "ast-grep scan --rule /etc/jobtrek/sw/ast-grep-rules/{}.yaml {} --json",
+        extension, file
+    )))
+    .unwrap()
+}
+
+/// run a bash command and return the output
+fn run_command(command: &str) -> String {
+    String::from_utf8(
+        Command::new("sh")
+            .arg("-c")
+            .arg(command)
+            .output()
+            .expect("failed to execute process")
+            .stdout,
+    )
+    .unwrap()
 }
