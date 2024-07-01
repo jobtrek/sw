42634a8e3b32e13fa2b6d442cabaaf3c840a4209
Rignchen <rinchendemort@gmail.com>
2024-07-01 12:04:59 +0200

    refactor: use an enum to recognize extensions

--- src/main.rs
+++ src/main.rs
@@ -43,26 +43,47 @@ structstruck::strike! {
 struct Args {
     #[clap(default_value = ".")]
     path: String,
-    #[clap(short, long, default_values_t = vec!["rs".to_string(), "js".to_string(), "ts".to_string()])]
-    extensions: Vec<String>,
+    #[clap(short, long)]
+    extensions: Vec<Extension>,
     #[clap(long)]
     silent: bool,
 }
 
+#[derive(Clone, Debug)]
+enum Extension {
+    Rs,
+    // Php,
+    Js,
+    Ts,
+    // Java,
+}
+impl Extension {
+    fn as_str(&self) -> &str {
+        match self {
+            Self::Rs => "rs",
+            // Self::Php => "php",
+            Self::Js => "js",
+            Self::Ts => "ts",
+            // Self::Java => "java",
+        }
+    }
+}
+impl std::str::FromStr for Extension {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "rs" => Ok(Self::Rs),
+            // "php" => Ok(Self::Php),
+            "js" => Ok(Self::Js),
+            "ts" => Ok(Self::Ts),
+            // "java" => Ok(Self::Java),
+            _ => Err(format!("invalid extension: {}", s)),
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
-    // extensions planed = "rs,php,js,ts,java"
-    let supported_extensions = vec!["rs", "js", "ts"];
-    if args
-        .extensions
-        .iter()
-        .any(|x| !supported_extensions.contains(&x.as_str()))
-    {
-        panic!(
-            "invalid extensions, only {:?} are allowed",
-            supported_extensions
-        );
-    }
     for extension in args.extensions {
         let extension = extension.as_str();
         let files = get_files(&args.path, extension);
