800a8c87df8d17bacf4897a8c61f1f97a2043ce6
Rignchen <rinchendemort@gmail.com>
2024-07-01 16:33:58 +0200

    feat: add test expectation files

---
+++ test_expected/typescript.ts
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} grades An array containing all grades
+ * @return {number} average with full precision
+ */
+export function computeAverage(grades) {
+    // Write your code here
+}
+
+export function first2OrThird(first, second, third) {
+    /*
+     * Add boolean operators between the inputs to make the test pass, keep the parameters in the same order as the function signature
+     * Write your logic here
+     */
+}
