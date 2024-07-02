63b24575cea23b3f6d9718368867ee050096534c
Nils Hofstetter <119439839+Rignchen@users.noreply.github.com>
2024-07-02 08:03:14 +0200

    feat: add files with the expectated result of the tests (#20)
    feat: add test expectation files
    
    refactor: move test and expected result in same folder

---
+++ test/expected/rust.rs
@@ -0,0 +1,41 @@
+pub fn first2_or_third(first: bool, second: bool, third: bool) -> bool {
+    /*
+     * Add boolean operators between the inputs to make the test pass, keep the parameters in the same order as the function signature
+     * Write your logic here
+     */
+    todo!()
+}
+
+pub enum Shape {
+    Circle {
+        // Write your logic here
+        todo!()
+    },
+    Rectangle {
+        // Write your logic here
+        todo!()
+    },
+}
+
+pub struct Token {
+    // Write your logic here
+    todo!()
+}
+
+/// Greet the person referred to in the `text` parameter.
+///
+/// ```
+/// assert_eq!(rust_ex::hello("world"), "Hello, world!");
+/// assert_eq!(rust_ex::hello("Rust"), "Hello, Rust!");
+/// ```
+/// Write your logic here
+pub fn hello(text: &str) -> String {
+    // Write here
+    format!("Hello, {}!", text)
+}
+
+pub fn tokenize(input: String) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+    // Write your code here
+    todo!()
+}
