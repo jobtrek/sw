673ae2ac0746933336fe3a60d5c42b2bd5017374
Rignchen <rinchendemort@gmail.com>
2024-07-03 13:53:07 +0200

    feat: add test cases for java

---
+++ test/expected/java.java
@@ -0,0 +1,11 @@
+public clas Test {
+    public static List<Token> tokenize(String input) {
+        // Write your code here
+    }
+    public static bool first_2_or_third(bool a, bool b, bool c) {
+        /*
+         * Write your code here
+         * Keep all input variable in the same order as in the function signature
+         */
+    }
+}
