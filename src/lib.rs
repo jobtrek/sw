7ac5345a34f0938098071c4aa16caa9ceaee0957
Rignchen <rinchendemort@gmail.com>
2024-07-09 10:56:16 +0200

    style: properly format code

--- src/lib.rs
+++ src/lib.rs
@@ -56,16 +56,25 @@ pub fn check_paths_exist(paths: &[String]) {
 /// assert_eq!(sw::get_files_per_extension("src/lib.rs", "rs", fd_bin_path), vec!["src/lib.rs".to_string()]);
 /// assert_eq!(sw::get_files_per_extension("src/lib.rs", "toml", fd_bin_path), Vec::<String>::new());
 /// ```
-pub fn get_files_per_extension(path: &str, extension: &str, fd_bin_path: Option<&str>) -> Vec<String> {
+pub fn get_files_per_extension(
+    path: &str,
+    extension: &str,
+    fd_bin_path: Option<&str>,
+) -> Vec<String> {
     let fail = |e| {
         panic!("failed to get metadata for {}: {}", path, e);
     };
     if std::fs::metadata(path).unwrap_or_else(fail).is_dir() {
-        return run_command(&format!("{} . {} -e {} --type f", fd_bin_path.unwrap_or("fd"), path, extension))
-            .split('\n')
-            .filter(|&x| !x.is_empty())
-            .map(|x| x.to_string())
-            .collect::<Vec<String>>();
+        return run_command(&format!(
+            "{} . {} -e {} --type f",
+            fd_bin_path.unwrap_or("fd"),
+            path,
+            extension
+        ))
+        .split('\n')
+        .filter(|&x| !x.is_empty())
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
     }
     if !path.ends_with(format!(".{}", extension).as_str()) {
         return vec![];
