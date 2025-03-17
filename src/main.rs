5149580af2025a83c65d68d0320b02dc10b7ad3a
fuzoh <info@bastiennicoud.ch>
2025-03-17 19:09:04 +0100

    feat: new wipe based ont token

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,5 @@
 use clap::Parser;
-use serde::{Deserialize, Serialize};
-use sw::{check_paths_exist, get_files_per_extension, run_command, unwrap_sw_error, SwError};
-
-structstruck::strike! {
-    /// structure of the json returned by ast-grep (only the useful parts)
-    #[strikethrough[derive(Serialize, Deserialize, Debug)]]
-    struct Program {
-        text: String,
-        range: struct {
-            start: struct {
-                line: u16,
-                column: u16,
-            },
-            end: struct {
-                line: u16,
-                column: u16,
-            }
-        },
-        #[serde(rename = "metaVariables")]
-        meta_variables: struct {
-            single: struct {
-                #[serde(rename = "COMMENT")]
-                comment: struct {
-                    text: String,
-                    // here we can directly refer to the range struct as we allready defined it
-                    range: Range
-                }
-            }
-        }
-    }
-}
+use sw::{check_paths_exist, get_files_per_extension, unwrap_sw_error};
 
 /// structure of the clap arguments
 /// sw [path = "."]
@@ -47,6 +17,8 @@ struct Args {
     silent: bool,
     #[clap(long)]
     fd_bin_path: Option<String>,
+    #[clap(long, default_value = "--sw-wipe--")]
+    matcher: String,
 }
 
 /// enum to represent all possible extensions
@@ -96,17 +68,15 @@ fn main() {
                 checked_files.push(file.clone());
 
                 if !args.silent {
-                    println!("{}", file);
+                    println!("Replacing in {}", file);
                 }
-                let parsed = unwrap_sw_error(get_removable_parts(extension, &file));
-                if parsed.is_empty() {
-                    // don't modyfy a file if it has nothing to remove
-                    continue;
-                }
-                unwrap_sw_error(match extension {
-                    "rs" => remove_parts(&file, &parsed, "todo!()"),
-                    _ => remove_parts(&file, &parsed, ""),
-                });
+
+                let file_content = std::fs::read_to_string(&file).expect("Cannot read file");
+
+                std::fs::write(file, match extension {
+                    "rs" => remove_parts(&file_content, &args.matcher, "todo!()"),
+                    _ => remove_parts(&file_content, &args.matcher, ""),
+                }).expect("Cannot write file");
             }
         }
     }
@@ -114,56 +84,26 @@ fn main() {
 
 /// remove the parts of the file that are defined in the given list of programs
 /// they are removed in reverse order to avoid changing the line numbers of the area that still haven't been removed
-fn remove_parts(file: &str, area_to_remove: &[Program], replace_with: &str) -> Result<(), SwError> {
-    let file_content = std::fs::read_to_string(file)?;
+fn remove_parts(file_content: &str, matcher: &str, replace_with: &str) -> String {
     // convert the content of the file from a string to a vector of strings (one string per line)
-    let mut file_content = file_content
+    let file_content = file_content
         .lines()
         .map(String::from)
         .collect::<Vec<String>>();
-    for area in area_to_remove.iter().rev() {
-        file_content.splice(
-            (area.meta_variables.single.comment.range.end.line as usize + 1)
-                ..=(area.range.end.line as usize - 1),
-            indent(
-                replace_with,
-                area.meta_variables.single.comment.range.start.column as usize,
-            ),
-        );
-    }
-    std::fs::write(file, file_content.join("\n"))?;
-    Ok(())
-}
-
-/// add the right amount of spaces to the beginning of each lines
-pub fn indent(lines: &str, spaces: usize) -> Vec<String> {
-    lines
-        .lines()
-        .map(|x| format!("{: >width$}{}", "", x, width = spaces))
-        .collect()
-}
-
-/// get the positions of the comments and block who define the part to remove
-fn get_removable_parts(extension: &str, file: &str) -> Result<Vec<Program>, SwError> {
-    match serde_json::from_str(&run_command(&format!(
-        "ast-grep scan --rule /etc/jobtrek/sw/ast-grep-rules/{}.yaml {} --json",
-        extension, file
-    ))?) {
-        Ok(x) => Ok(x),
-        Err(e) => Err(SwError::AstGrepParseError(e)),
-    }
-}
-
-// tests
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_indent() {
-        assert_eq!(indent("a\nb", 2), vec!["  a", "  b"]);
-        assert_eq!(indent("a\nb", 0), vec!["a", "b"]);
-        assert_eq!(indent("a", 4), vec!["    a"]);
-        assert_eq!(indent("", 2), Vec::<String>::new());
-    }
+    
+    // iterate through all lines in reverse and remove all lines between lines that contains matcher
+    let mut remove = false;
+    let wiped_file: Vec<&str> = file_content.iter().filter_map(|line| {
+        if line.contains(matcher) {
+            remove = !remove;
+            if !remove {
+                return Some(replace_with)
+            }
+        }
+        if remove {
+            return None
+        }
+        Some(line)
+    }).collect();
+    wiped_file.join("\n")
 }
