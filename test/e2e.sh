a333a2725a761e1f065f5e01c6d0f3ee44bb044f
Rignchen <rinchendemort@gmail.com>
2025-10-16 11:08:23 +0200

    perf: don't store fd's result in variable

--- test/e2e.sh
+++ test/e2e.sh
@@ -9,20 +9,16 @@ else
 fi
 export FD_CMD
 
-FILES="$($FD_CMD -t f -e sh)"
 declare -A FAILED_FILES
-
 while read -r FILE; do
 	echo "Running $FILE:"
-
 	# Run the script and print its output with 1 indentation level
 	bash "$FILE" 2>&1 | sed 's/^/\t/'
 	LAST_EXIT_CODE=${PIPESTATUS[0]}
-
 	if [ $LAST_EXIT_CODE -ne 0 ]; then
 		FAILED_FILES["$FILE"]=$LAST_EXIT_CODE
 	fi
-done < <(echo "$FILES")
+done < <($FD_CMD -t f -e sh)
 
 if [ ${#FAILED_FILES[@]} -eq 0 ]; then
 	echo "All tests passed! ✅"
