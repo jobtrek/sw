224e1ec3f31af23bdcab141ce838c12faabf8587
Rignchen <rinchendemort@gmail.com>
2025-10-16 11:05:26 +0200

    test: run cleanup even if program crashes

--- test/e2e/run_on_test_should_be_the_same_as_expected.sh
+++ test/e2e/run_on_test_should_be_the_same_as_expected.sh
@@ -3,11 +3,17 @@
 set -euo pipefail
 cd "$(dirname "$0")/.."
 
+cleanup() {
+	git restore test
+}
+
+trap cleanup EXIT
+
 DIFF_TOOL="${DIFF_TOOL:-diff}"
 
 cargo run -- test --fd-bin-path "$FD_CMD"
 DIFF="$($DIFF_TOOL test expected)"
-git restore test
+cleanup
 if [ -n "$DIFF" ]; then
 	echo "Differences found between test and expected:"
 	echo "$DIFF" | sed 's/^/\t/'
